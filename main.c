5b499395e66cf6b680747960ffbaa09d9f5574eb
KAI <gigikie@gmail.com>
2014-02-10 22:28:16 +0800

    done: Assignment 1 with bonus

---
+++ main.c
@@ -0,0 +1,49 @@
+#include "stdio.h"
+
+// EE2024 Assignment 1
+// (C) CK Tham, ECE, NUS, 2014
+
+// Optimization routine written in assembly language
+// Input parameters (signed integers):
+//     xi : integer version of x
+//     a, b, c : integer coefficients of f(x)
+// Output : returns solution x (signed integer)
+extern int optimize(int xi, int a, int b, int* cnt);
+
+int main(void)
+{
+    int a=2, b=-3, c=3, xsoli, cnt = 0;
+    float fp, x, xprev, xsol, change, lambda=0.2;
+
+    printf("Initial value of x (press [Enter] after keying in): "); // try x0 = -12.35
+    scanf("%f", &x);
+
+//  ARM ASM & Integer version
+    printf("ARM ASM & Integer version:\n");
+    xsoli = optimize(x,a,b,&cnt);
+    xsol = xsoli;
+    printf("xsol : %f \n",((float)xsol)/100);
+    printf("number of iterations: %d\n", cnt);
+
+//  C & Floating Point version
+  	printf("C & Floating point version:\n",x);
+    while (1)
+    {
+    	fp = 2*a*x + b;
+
+    	xprev = x;
+    	change = -lambda*fp;
+    	x += change;
+
+    	printf("x: %f, fp: %f, change: %f\n", x, fp, change);
+    	if (x==xprev) break;
+    }
+
+    // Enter an infinite loop, just incrementing a counter
+	// This is for convenience to allow registers, variables and memory locations to be inspected at the end
+	volatile static int loop = 0;
+	while (1) {
+		loop++;
+	}
+}
+
---
+++ optimize.s
@@ -0,0 +1,34 @@
+ 	.syntax unified
+ 	.cpu cortex-m3
+ 	.thumb
+ 	.align 2
+ 	.global	optimize
+ 	.thumb_func
+@  EE2024 Assignment 1 skeleton
+@  CK Tham, ECE, NUS, 2014
+optimize:
+@ at the beginning, R0 = x as Integer, R1 = a, R2 = b, R3 = addr of cnt (counter)
+@ Init
+	PUSH {R4-R6}	@ back up other registers R4 ~ R7
+	MOV R4, #100	@ R4 = Scale Factor
+	MOV R5, #5		@ R5 = 1 / Lambda
+	MUL R0, R4		@ R0 *= R4, which is x *= 100
+	MUL R2, R4		@ R2 *= R4, which is b *= 100
+
+@ now, R0 = 100 * x as Integer, R1 = a, R2 = b * 100, R3 = addr of cnt (counter)
+	LDR R4, [R3]	@ load cnt's value into R4
+loop:
+	ADD R4, #1		@ ++R4 for every loop
+	MUL R6, R0, R1	@ R6 = R0 * R1, which is R6 = a * x
+	ADD R6, R6		@ R6 += R6, which is R6 = 2 * a * x
+	ADD R6, R2		@ R6 += R2, which is R6 = d(f) = 2 * a * x + 100 * b
+	SDIV R6, R5		@ R6 /= R5, which is d(f) / lambda
+	CMP R6, #0		@ Compare R6 with zero
+	BEQ DONE		@ if this branch is equal, go to DONE
+	SUB R0, R6		@ else R0 -= R6, which is R0 = x` =  x - d(f) / lambda
+b loop				@ while(1), next loop
+
+DONE:
+	STR R4, [R3]	@ store R4 back to R3's address, which is *cnt = R4
+	POP {R4-R6}		@ restore registers R4 ~ R7
+	BX LR			@ return R0, AKA x
