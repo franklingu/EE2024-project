217f2036d591eea96524c3a766155ba3cdac2dab
franklingu <franklingujunchao@gmail.com>
2014-03-03 00:56:43 +0800

    create project assignment 2

--- demo/Debug/src/cr_startup_lpc17.d
+++
@@ -1,4 +0,0 @@
-src/cr_startup_lpc17.d src/cr_startup_lpc17.o: ../src/cr_startup_lpc17.c \
-  C:\Users\Giki\Documents\lpcxpresso_3.6.3_317\workspace\Lib_CMSISv1p30_LPC17xx\inc/system_LPC17xx.h
-
-C:\Users\Giki\Documents\lpcxpresso_3.6.3_317\workspace\Lib_CMSISv1p30_LPC17xx\inc/system_LPC17xx.h:
